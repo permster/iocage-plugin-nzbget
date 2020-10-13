c4cca0bd0696afe5a2516ac94adee6483761fc9f
Travis <tjgollih@gmail.com>
2020-10-13 00:15:31 -0700

    Initial commit

---
+++ post_install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ln -s /usr/local/bin/python2.7 /usr/local/bin/python
+ln -s /usr/local/bin/python2.7 /usr/local/bin/python2
+
+#Install pip for Python package management
+python -m ensurepip
+pip install --upgrade pip
+
+fetch https://nzbget.net/download/nzbget-latest-bin-freebsd.run -o /usr/local/
+
+sh /usr/local/nzbget-latest-bin-freebsd.run --destdir /usr/local/nzbget
+rm /usr/local/nzbget-latest-bin-freebsd.run
+
+pw user add nzbget -c nzbget -u 504 -d /nonexistent -s /usr/bin/nologin
+chown -R nzbget:nzbget /usr/local/nzbget
+
+#Add group for access to mounted storage
+pw groupadd -n mnt -g 1002
+pw groupmod 1002 -m 504
+
+chmod u+x /etc/rc.d/nzbget
+sysrc -f /etc/rc.conf nzbget_enable="YES"
+service nzbget start 2>/dev/null
+
+echo "NZBGet successfully installed" > /root/PLUGIN_INFO
